350fa2cc4c30b80177ac428f52237076d7b0ae81
felipeCast <castfelipe10@gmail.com>
2024-02-16 16:53:22 -0300

    feat: Adicionando slide de imagens na tela detalhes

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -1,56 +1,110 @@
 import { useState, useEffect } from "react";
-import { View, Text } from "react-native";
-import { useRoute } from "@react-navigation/native";
+import { View, Text, Dimensions } from "react-native";
 import './style.js';
 
-import { Container, BackgroundSlider, Rating, RatingText, Title } from "./style.js";
+import { useNavigation } from "@react-navigation/native";
+import { useRoute } from "@react-navigation/native";
+
+
+import { Container, ContainerBanner, Rating, RatingText, Title, ImageContainer, ImagesList, ContainerButtons, ReturnButton, FavoriteButton, ContainerContent } from "./style.js";
 
 import api from "../../services/api.js";
 
 import { AntDesign } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
-export default function Details(){
+const windowWidth = Dimensions.get('window').width;
+
+export default function Details() {
 
     const route = useRoute();
-    const data  = route.params?.item;
+    const data = route.params?.item;
+    const navigation = useNavigation();
 
     const [content, setContent] = useState(null);
-
-
+    const [backgroundImages, setBackgroundImages] = useState([]);
+    
     useEffect(() => {
-
-        async function loadDetails(){
+        async function loadDetails() {
 
             try {
+
                 const response = await api.get(`games/${data.slug}?key=96ffa22939174620840e464e6200055c`);
                 setContent(response.data);
+
+                setBackgroundImages([response.data.background_image, response.data.background_image_additional]);
+
             } catch (error) {
                 console.error("Erro ao carregar detalhes:", error);
             }
+
         }
 
-        if(!content){
+        if (!content) {
             loadDetails();
         }
-        
+    }, [data.slug, content]);
 
-    }, [data.slug])
-
-    return(
+    return (
         <Container>
 
-            {content ? ( 
-                <>
-                    <Title>{content.name}</Title>
+            <ContainerBanner>
+
+                <ContainerButtons>
+
+                    <ReturnButton onPress={() => navigation.goBack()}>
+                        <AntDesign name="arrowleft" size={24} color="white" />
+                    </ReturnButton>
+                    
+                    <FavoriteButton onPress={() => navigation.navigate('Favorites')}>
+                        <Feather name="bookmark" size={24} color="white" />
+                    </FavoriteButton>
+
+                </ContainerButtons>
+
+                <ImagesList
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    data={backgroundImages}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => (
+                        <ImageContainer
+                            source={{ uri: item }}
+                            windowWidth={windowWidth}
+                        />
+                    )}
+                />
+            </ContainerBanner>
+
+            {content ? (
+
+                <ContainerContent>
+
                     <Rating>
                         <AntDesign name="star" size={20} color="#FABB1E" />
                         <RatingText>{data.rating.toFixed(1)}/ 5</RatingText>
                     </Rating>
-                </>
+
+                    <Text style={{color: 'white', fontSize: 18, fontWeight: 'bold', marginTop: 10}}>{content.name}</Text>
+
+                    <Title>Genres:</Title>
+
+                    {/* Renderize os gêneros aqui */}
+        {content.genres.map(genre => (
+          <Text key={genre.id}>
+            <Text style={{color: 'white'}}>{genre.name}</Text>
+          </Text>
+        ))}
+
+                </ContainerContent>
+
             ) : (
-                <Text>Carregando...</Text>
+                <Text style={{textAlign: 'center', color: 'white'}}>Carregando...</Text>
             )}
 
+
+
         </Container>
-    )
+    );
+
 }
--- src/pages/details/style.js
+++ src/pages/details/style.js
@@ -2,23 +2,60 @@ import styled from "styled-components/native";
 
 export const Container = styled.SafeAreaView`
     flex: 1;
-    width: 100%;
+    background-color: #050B18;
 `;
 
-export const BackgroundSlider = styled.FlatList`
-    max-height: 100%;
-    width: 100%;
-    background-color: gray;
+export const ContainerContent = styled.SafeAreaView`
+    padding: 20px;
+`;
+
+export const ImageContainer = styled.Image`
+    width: ${({ windowWidth }) => windowWidth}px;
+    height: 200px;
+`;
+
+export const ImagesList = styled.FlatList`
+  position: absolute;
+`;
+
+export const ContainerBanner = styled.View`
+  height: 200px;
+`;
+
+export const ContainerButtons = styled.View`
+  z-index: 99;
+  flex-direction: row;
+  padding: 10px;
+  justify-content: space-between;
+  gap: 20px;
+`;
+
+export const FavoriteButton = styled.TouchableOpacity`
+    background-color: #050B18;
+    padding: 10px;
+    border-radius: 50px;
+`;
+
+export const ReturnButton = styled.TouchableOpacity`
+    background-color: #050B18;
+    padding: 10px;
+    border-radius: 50px;
 `;
 
 export const Rating = styled.View`
-
+    flex-direction: row;
+    gap: 6px;
 `;
 
 export const RatingText = styled.Text`
-
+    color: white;
+    margin-right: 10px;
+    font-size: 16px;
 `;
 
 export const Title = styled.Text`
-
+    margin-top: 20px;
+    color: white;
+    font-weight: bold;
+    font-size: 25px;
 `;
