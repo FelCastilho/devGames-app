c09796e43d5990843537210ac60cb50f3f06bdac
felipeCast <castfelipe10@gmail.com>
2024-02-17 11:54:15 -0300

    feat: Adicionado tela de loading na Home e em details

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { View, Text, Dimensions } from "react-native";
+import { View, Text, Dimensions, ActivityIndicator } from "react-native";
 import './style.js';
 
 import { useNavigation } from "@react-navigation/native";
@@ -23,16 +23,18 @@ export default function Details() {
 
     const [content, setContent] = useState(null);
     const [backgroundImages, setBackgroundImages] = useState([]);
-    
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
+
         async function loadDetails() {
 
             try {
-
                 const response = await api.get(`games/${data.slug}?key=96ffa22939174620840e464e6200055c`);
                 setContent(response.data);
 
                 setBackgroundImages([response.data.background_image, response.data.background_image_additional]);
+                setLoading(false)
 
             } catch (error) {
                 console.error("Erro ao carregar detalhes:", error);
@@ -43,68 +45,74 @@ export default function Details() {
         if (!content) {
             loadDetails();
         }
+
     }, [data.slug, content]);
 
-    return (
-        <Container>
+    if(loading){
 
-            <ContainerBanner>
+        return(
+            <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1, backgroundColor: '#050B18'}}>
+                <ActivityIndicator size={24} color="#FF455F"/>
+            </View>
+        )
 
-                <ContainerButtons>
+    }else{
 
-                    <ReturnButton onPress={() => navigation.goBack()}>
-                        <AntDesign name="arrowleft" size={24} color="white" />
-                    </ReturnButton>
-                    
-                    <FavoriteButton onPress={() => navigation.navigate('Favorites')}>
-                        <Feather name="bookmark" size={24} color="white" />
-                    </FavoriteButton>
+        return (
 
-                </ContainerButtons>
-
-                <ImagesList
-                    horizontal
-                    showsHorizontalScrollIndicator={false}
-                    data={backgroundImages}
-                    keyExtractor={(item, index) => index.toString()}
-                    renderItem={({ item }) => (
-                        <ImageContainer
-                            source={{ uri: item }}
-                            windowWidth={windowWidth}
-                        />
-                    )}
-                />
-            </ContainerBanner>
-
-            {content ? (
-
-                <ContainerContent>
-
-                    <Rating>
-                        <AntDesign name="star" size={20} color="#FABB1E" />
-                        <RatingText>{data.rating.toFixed(1)}/ 5</RatingText>
-                    </Rating>
-
-                    <Text style={{color: 'white', fontSize: 18, fontWeight: 'bold', marginTop: 10}}>{content.name}</Text>
-
-                    <Title>Genres:</Title>
-
-                    {/* Renderize os gêneros aqui */}
-        {content.genres.map(genre => (
-          <Text key={genre.id}>
-            <Text style={{color: 'white'}}>{genre.name}</Text>
-          </Text>
-        ))}
-
-                </ContainerContent>
-
-            ) : (
-                <Text style={{textAlign: 'center', color: 'white'}}>Carregando...</Text>
-            )}
-
-
-
-        </Container>
-    );
+            <Container>
+    
+                <ContainerBanner>
+    
+                    <ContainerButtons>
+    
+                        <ReturnButton onPress={() => navigation.goBack()}>
+                            <AntDesign name="arrowleft" size={24} color="white" />
+                        </ReturnButton>
+    
+                        <FavoriteButton onPress={() => navigation.navigate('Favorites')}>
+                            <Feather name="bookmark" size={24} color="white" />
+                        </FavoriteButton>
+    
+                    </ContainerButtons>
+    
+                    <ImagesList
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                        data={backgroundImages}
+                        keyExtractor={(item, index) => index.toString()}
+                        renderItem={({ item }) => (
+                            <ImageContainer
+                                source={{ uri: item }}
+                                windowWidth={windowWidth}
+                            />
+                        )}
+                    />
+                </ContainerBanner>
+    
+                {content && (
+    
+                    <ContainerContent>
+    
+                        <Rating>
+                            <AntDesign name="star" size={20} color="#FABB1E" />
+                            <RatingText>{data.rating.toFixed(1)}/ 5</RatingText>
+                        </Rating>
+    
+                        <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold', marginTop: 10 }}>{content.name}</Text>
+    
+                        <Title>Genres:</Title>
+    
+                    </ContainerContent>
+    
+                )}
+    
+    
+    
+            </Container>
+        );
+    }
+    
+    
 
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import './style.js';
+import { View, ActivityIndicator } from 'react-native';
 import { Header } from "../../components/header/index.js";
 import { Container, ListGenre, ContainerGames, Title, ListGames } from "./style.js";
 
@@ -12,6 +13,7 @@ export default function Home(){
 
     const [genres, setGenres] = useState([]);
     const [games, setGames] = useState([]);
+    const [ loading, setLoading ] = useState(true);
     
     useEffect(() => {
 
@@ -20,45 +22,58 @@ export default function Home(){
             const response = await api.get('genres?key=96ffa22939174620840e464e6200055c');
 
             setGenres(response.data.results.slice(0, 5));
+            setLoading(false);
         }
 
         async function loadGames(){
             const response = await api.get('games?page_size=5&key=96ffa22939174620840e464e6200055c')
 
             setGames(response.data.results.slice(0,5))
+            setLoading(false);
         }
 
         loadGenres();
         loadGames();
-
     }, [])
 
-    return(
+    if(loading){
+        return(
+            <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1, backgroundColor: '#050B18'}}>
+             <ActivityIndicator size={24} color="#FF455F"/>
+            </View>
+        )
         
-        <Container>
+    }else{
 
-            <Header/>
-
-            <ListGenre
-            data={genres}
-            renderItem={({ item }) => <Genres data={item}/>}
-            keyExtractor={item => String(item.id)}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            />
-
-            <ContainerGames>
-                
-                <Title>Trending games</Title>
-
-                <ListGames
-                data={games}
-                renderItem={({item}) => <Games data={item}/>}
+        return(
+        
+            <Container>
+    
+                <Header/>
+    
+                <ListGenre
+                data={genres}
+                renderItem={({ item }) => <Genres data={item}/>}
                 keyExtractor={item => String(item.id)}
+                horizontal
+                showsHorizontalScrollIndicator={false}
                 />
+    
+                <ContainerGames>
+                    
+                    <Title>Trending games</Title>
+    
+                    <ListGames
+                    data={games}
+                    renderItem={({item}) => <Games data={item}/>}
+                    keyExtractor={item => String(item.id)}
+                    />
+    
+                </ContainerGames>
+    
+            </Container>
+        )
+    }
 
-            </ContainerGames>
-
-        </Container>
-    )
+    
 }
